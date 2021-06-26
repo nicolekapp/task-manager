75857b7e2cae8a0f8b1c15b128f56c3a37755352
tomasf10 <tomasfrechou@uca.edu.ar>
2021-06-26 14:37:04 -0300

    feature/TASKMANAGER-02: react-redux configured

---
+++ src/ducks/duckReducerModel.js
@@ -0,0 +1,26 @@
+// What is Redux Ducks?
+// https://medium.com/@matthew.holman/what-is-redux-ducks-46bcb1ad04b7
+
+// Actions
+const ACTION_NAME = "ACTION_NAME";
+
+// Action Creators
+export const action = (propName) => (dispatch) => dispatch({ type: ACTION_NAME, propName });
+
+// State
+const initialState = {};
+
+// Reducer
+const ducksReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ACTION_NAME:
+      return {
+        ...state,
+        // state: action.propName
+      };
+    default:
+      return state;
+  }
+};
+
+export default ducksReducer;
