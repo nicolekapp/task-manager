87e2dd6449bf829ad9700d2c83b4a3a9aa08eb7e
Salvador Steverlynck <fernandosteverlynck@uca.edu.ar>
2021-07-01 20:38:48 -0300

    Agregado en branch grupo 3 frontend

---
+++ src/components/squad3/psa/src/ImportGoogle.js
@@ -0,0 +1,12 @@
+import TopNav from './TopNav';
+import BodyGoogle from './BodyGoogle';
+import Footer from "./Footer";
+
+export default function App() {
+  return (<div>
+    <TopNav/>
+    <BodyGoogle/>
+    <Footer/>
+    </div>
+  );
+}
---
+++ src/components/squad3/psa/src/help.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default class Body extends React.Component{
+    render(){
+        return(
+            <div>
+                <p>
+                    Llame al 111 para solicitar ayuda
+                </p>
+            </div>
+        )
+    }
+}
