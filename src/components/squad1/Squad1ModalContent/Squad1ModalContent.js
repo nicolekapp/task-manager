c8487290eb9b72c6fd213de59c1c7f184e7f3ae8
tomasf10 <tomasfrechou@uca.edu.ar>
2021-07-01 14:42:06 -0300

    feature/TASKMANAGER-SI2-16: minor change in the squad1 modal content

--- src/components/squad1/Squad1ModalContent/Squad1ModalContent.js
+++ src/components/squad1/Squad1ModalContent/Squad1ModalContent.js
@@ -1,5 +1,7 @@
 import TextField from "@material-ui/core/TextField";
 import { makeStyles } from "@material-ui/core/styles";
+import Grid from "@material-ui/core/Grid";
+
 import React from "react";
 
 const Squad1ModalContent = () => {
@@ -25,73 +27,77 @@ const Squad1ModalContent = () => {
 
   return (
     <form className={classes.root} noValidate autoComplete="off">
-      <div>
-        <TextField
-          required
-          id="standard-required"
-          label="Nombre tarea 2"
-          defaultValue="Default Value"
-          className={classes.textField}
-          value={values.title}
-          onChange={handleChange("title")}
-          helperText={error2 ? "The task needs to have a title" : "Perfect!"}
-          error={error2}
-        />
+      <Grid container direction="column">
+        <Grid item>
+          <TextField
+            required
+            id="standard-required"
+            label="Nombre tarea 2"
+            defaultValue="Default Value"
+            className={classes.textField}
+            value={values.title}
+            onChange={handleChange("title")}
+            helperText={error2 ? "The task needs to have a title" : "Perfect!"}
+            error={error2}
+          />
+        </Grid>
 
-        <TextField
-          id="task description"
-          label="Task description"
-          type="task description"
-          multiline
-          rowsMax={10}
-          value={values.description}
-          onChange={handleChange("description")}
-          defaultValue="Default Value"
-          fullWidth
-          variant="outlined"
-          helperText={
-            error ? "The task needs to have a descirption" : "Perfect!"
-          }
-          error={error}
-        />
+        <Grid item>
+          <TextField
+            id="task description"
+            label="Task description"
+            type="task description"
+            multiline
+            rowsMax={10}
+            value={values.description}
+            onChange={handleChange("description")}
+            defaultValue="Default Value"
+            fullWidth
+            variant="outlined"
+            helperText={error ? "The task needs to have a descirption" : "Perfect!"}
+            error={error}
+          />
+        </Grid>
+        <Grid container item>
+          <Grid item>
+            <TextField
+              id="standard-helperText"
+              label="Tiempo de duracion : Dias"
+              defaultValue="Default Value"
+              className={classes.textField}
+              value={values.durationDays}
+              onChange={handleChange("durationDays")}
+              helperText={
+                error3 ? "The task needs to have an estimated completion time" : "Perfect!"
+              }
+              error={error3}
+              type="number"
+              InputLabelProps={{
+                shrink: true,
+              }}
+            />
+          </Grid>
 
-         <TextField
-         id="standard-helperText"
-         label="Tiempo de duracion : Dias"
-         defaultValue="Default Value"
-         className={classes.textField}
-         value={values.durationDays}
-         onChange={handleChange("durationDays")}
-         helperText={
-           error3
-             ? "The task needs to have an estimated completion time"
-             : "Perfect!"
-         }
-         error={error3}
-          type="number"
-          InputLabelProps={{
-            shrink: true,
-          }}
-        />
-        <TextField
-         id="standard-helperText"
-         label="Tiempo de duracion : Horas"
-         defaultValue="Default Value"
-         className={classes.textField}
-         value={values.durationHours}
-         onChange={handleChange("durationHours")}
-         helperText={
-           error3
-             ? "The task needs to have an estimated completion time"
-             : "Perfect!"
-         }
-         error={error3}
-          type="number"
-          InputLabelProps={{
-            shrink: true,
-          }}
-        />
-      </div>
+          <Grid item>
+            <TextField
+              id="standard-helperText"
+              label="Tiempo de duracion : Horas"
+              defaultValue="Default Value"
+              className={classes.textField}
+              value={values.durationHours}
+              onChange={handleChange("durationHours")}
+              helperText={
+                error3 ? "The task needs to have an estimated completion time" : "Perfect!"
+              }
+              error={error3}
+              type="number"
+              InputLabelProps={{
+                shrink: true,
+              }}
+            />
+          </Grid>
+        </Grid>
+      </Grid>
     </form>
   );
 };
