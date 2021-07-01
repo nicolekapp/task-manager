51b80922aa38d974070fd7b9cc5f308df4e30147
tomasf10 <tomasfrechou@uca.edu.ar>
2021-07-01 17:15:18 -0300

    feature/TASKMANAGER-SI2-16: Date pickers have been added to the modal

--- src/components/squad1/Squad1ModalContent/Squad1ModalContent.js
+++ src/components/squad1/Squad1ModalContent/Squad1ModalContent.js
@@ -1,17 +1,29 @@
-import TextField from "@material-ui/core/TextField";
-import { makeStyles } from "@material-ui/core/styles";
-import Grid from "@material-ui/core/Grid";
+import React, { useCallback, useState } from "react";
 
-import React from "react";
+import TextField from "@material-ui/core/TextField";
+import Grid from "@material-ui/core/Grid";
+import { MuiPickersUtilsProvider, KeyboardDatePicker } from "@material-ui/pickers";
+import DateFnsUtils from "@date-io/date-fns";
 
 const Squad1ModalContent = () => {
-  const classes = styles();
-  const [values, setValues] = React.useState(
+  const [values, setValues] = useState(
     {
       name: "",
     },
     "Controlled"
   );
+
+  const [startDate, setStartDate] = React.useState(new Date());
+  const [endDate, setEndDate] = React.useState(new Date());
+
+  const handleStartDateChange = useCallback((date) => {
+    setStartDate(date);
+  }, []);
+
+  const handleEndDateChange = useCallback((date) => {
+    setEndDate(date);
+  }, []);
+
   const handleChange = (description, title, durationDays, durationHours) => (event) => {
     setValues({
       ...values,
@@ -23,98 +35,66 @@ const Squad1ModalContent = () => {
   };
   const error = values.description === "";
   const error2 = values.title === "";
-  const error3 = values.duration === "";
 
   return (
-    <form className={classes.root} noValidate autoComplete="off">
-      <Grid container direction="column">
+    <Grid container direction="column" spacing={2}>
+      <Grid item>
+        <TextField
+          aria-required
+          label="Nombre de la tarea"
+          value={values.title}
+          onChange={handleChange("title")}
+          fullWidth
+          variant="outlined"
+          helperText={error2 && "The task needs to have a title"}
+          error={error2}
+        />
+      </Grid>
+
+      <Grid item>
+        <TextField
+          label="Description"
+          multiline
+          rowsMax={10}
+          value={values.description}
+          onChange={handleChange("description")}
+          fullWidth
+          variant="outlined"
+          helperText={error && "The task needs to have a descirption"}
+          error={error}
+        />
+      </Grid>
+      <Grid container item spacing={4} justify="center">
         <Grid item>
-          <TextField
-            required
-            id="standard-required"
-            label="Nombre tarea 2"
-            defaultValue="Default Value"
-            className={classes.textField}
-            value={values.title}
-            onChange={handleChange("title")}
-            helperText={error2 ? "The task needs to have a title" : "Perfect!"}
-            error={error2}
-          />
+          <MuiPickersUtilsProvider utils={DateFnsUtils}>
+            <KeyboardDatePicker
+              disableToolbar
+              variant="inline"
+              format="dd/MM/yyyy"
+              margin="normal"
+              label="Fecha de inicio"
+              value={startDate}
+              onChange={handleStartDateChange}
+            />
+          </MuiPickersUtilsProvider>
         </Grid>
 
         <Grid item>
-          <TextField
-            id="task description"
-            label="Task description"
-            type="task description"
-            multiline
-            rowsMax={10}
-            value={values.description}
-            onChange={handleChange("description")}
-            defaultValue="Default Value"
-            fullWidth
-            variant="outlined"
-            helperText={error ? "The task needs to have a descirption" : "Perfect!"}
-            error={error}
-          />
-        </Grid>
-        <Grid container item>
-          <Grid item>
-            <TextField
-              id="standard-helperText"
-              label="Tiempo de duracion : Dias"
-              defaultValue="Default Value"
-              className={classes.textField}
-              value={values.durationDays}
-              onChange={handleChange("durationDays")}
-              helperText={
-                error3 ? "The task needs to have an estimated completion time" : "Perfect!"
-              }
-              error={error3}
-              type="number"
-              InputLabelProps={{
-                shrink: true,
-              }}
+          <MuiPickersUtilsProvider utils={DateFnsUtils}>
+            <KeyboardDatePicker
+              disableToolbar
+              variant="inline"
+              format="dd/MM/yyyy"
+              margin="normal"
+              label="Fecha de finalización"
+              value={endDate}
+              onChange={handleEndDateChange}
             />
-          </Grid>
-
-          <Grid item>
-            <TextField
-              id="standard-helperText"
-              label="Tiempo de duracion : Horas"
-              defaultValue="Default Value"
-              className={classes.textField}
-              value={values.durationHours}
-              onChange={handleChange("durationHours")}
-              helperText={
-                error3 ? "The task needs to have an estimated completion time" : "Perfect!"
-              }
-              error={error3}
-              type="number"
-              InputLabelProps={{
-                shrink: true,
-              }}
-            />
-          </Grid>
+          </MuiPickersUtilsProvider>
         </Grid>
       </Grid>
-    </form>
+    </Grid>
   );
 };
-
-const styles = makeStyles((theme) => ({
-  root: {
-    "& .MuiTextField-root": {
-      margin: theme.spacing(3),
-      display: "flex",
-      flexWrap: "wrap",
-      marginLeft: theme.spacing(1),
-      marginRight: theme.spacing(3),
-    },
-  },
-  textField: {
-    width: "26ch",
-  },
-}));
 
 export default Squad1ModalContent;
