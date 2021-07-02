0c9dd799d17d8376a99e536cccfef8489adde522
Fs <fede.stulich@gmail.com>
2021-07-02 02:29:54 -0300

    Historial Completo

---
+++ src/components/squad2/Squad2App/TotalTime/TotalTime.js
@@ -0,0 +1,27 @@
+import React, {Component} from "react";
+
+
+export default class TotalTime extends Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+          time: "",
+        }
+      }    
+      componentDidMount(){
+
+      }
+    fetchTotalTime(){
+        fetch('https://is3-squad2-tiempos.herokuapp.com/timeTask/getTotalTime/100')
+        .then(data => this.setState({time: data}))
+    }
+    
+  render(){
+    return(
+        <div style={{marginRight:30}}>
+            Tiempo total dedicado: {this.state.time}
+        </div>           
+
+    )
+  }
+}
