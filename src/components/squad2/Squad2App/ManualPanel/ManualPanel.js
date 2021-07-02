5836f5178d1b7caa7ce2b5c92fd96a66aaa28ace
fede2442 <fede.sarquis@hotmail.com>
2021-07-01 23:24:19 -0300

    feature/TASKMANAGER-10 todas las apis conectadas

--- src/components/squad2/Squad2App/ManualPanel/ManualPanel.js
+++ src/components/squad2/Squad2App/ManualPanel/ManualPanel.js
@@ -2,22 +2,31 @@ import React, { Fragment, useState } from "react";
 //import PropTypes from "prop-types";
 import TimePicker from "../TimePicker/TimePicker";
 import Button from "@material-ui/core/Button";
+import * as utils from '../utils';
 
 //import { Typography } from "@material-ui/core";
 //import styles from "./styles";
 
 const ManualPanel = ({ manual }) => {
     const [manualTime, setManualTime] = useState("");
+    const {convertToHours,convertToMinutes,convertToSeconds} = utils;
 
     const postTime = () => {
+        var hours = parseInt(manualTime.slice(0, 2), 10);
+        var minutes = parseInt(manualTime.slice(3, 5),10);
+
         const requestOptions = {
             headers: { 'Content-Type': 'application/json' },
             method: 'POST',
-            body: JSON.stringify({ id: 52, nombre: 'nicole' })
+            body: JSON.stringify({
+                "horas": hours,
+                "minutos": minutes,
+                "segundos": 0,
+                "task": 200
+              })
         };
-        fetch('https://squad2-tiempos.herokuapp.com/Manual/prueba', requestOptions)
-            .then(response => response.json())
-            .then(json => console.log(json, 'RESPONSEEEE'));
+
+        fetch('https://is3-squad2-tiempos.herokuapp.com/Manual/register', requestOptions);
     }
 
     const addManual = () => postTime();
---
+++ src/components/squad2/Squad2App/utils.js
@@ -0,0 +1,39 @@
+
+export const convertHMS = (value) =>  {
+    const sec = parseInt(value, 10); // convert value to number if it's string
+    let hours   = Math.floor(sec / 3600); // get hours
+    let minutes = Math.floor((sec - (hours * 3600)) / 60); // get minutes
+    let seconds = sec - (hours * 3600) - (minutes * 60); //  get seconds
+    // add 0 if value < 10; Example: 2 => 02
+    if (hours   < 10) {hours   = "0"+hours;}
+    if (minutes < 10) {minutes = "0"+minutes;}
+    if (seconds < 10) {seconds = "0"+seconds;}
+    return hours+':'+minutes+':'+seconds; // Return is HH : MM : SS
+}
+
+export const convertToHours = (value) =>  {
+    const sec = parseInt(value, 10); 
+    let hours   = Math.floor(sec / 3600); // get hours
+    let minutes = Math.floor((sec - (hours * 3600)) / 60); // get minutes
+    let seconds = sec - (hours * 3600) - (minutes * 60); //  get seconds
+    if (hours   < 10) {hours   = "0"+hours;}
+    return hours;
+}
+
+export const convertToMinutes = (value) =>  {
+    const sec = parseInt(value, 10); // convert value to number if it's string
+    let hours   = Math.floor(sec / 3600); // get hours
+    let minutes = Math.floor((sec - (hours * 3600)) / 60); // get minutes
+    let seconds = sec - (hours * 3600) - (minutes * 60); //  get seconds
+    if (minutes < 10) {minutes = "0"+minutes;}
+    return minutes; // Return is HH : MM : SS
+}
+
+export const convertToSeconds = (value) =>  {
+    const sec = parseInt(value, 10); 
+    let hours   = Math.floor(sec / 3600); // get hours
+    let minutes = Math.floor((sec - (hours * 3600)) / 60); // get minutes
+    let seconds = sec - (hours * 3600) - (minutes * 60); //  get seconds
+    if (seconds < 10) {seconds = "0"+seconds;}
+    return seconds; // Return is HH : MM : SS
+}
